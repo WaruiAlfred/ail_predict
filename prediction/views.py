0b0cf098331035a47e06179430fb551d81c41b95
Alfred Warui <alfredwarui17@gmail.com>
2021-11-03 11:01:09 +0300

    create home and register view functions and their urls

--- prediction/views.py
+++ prediction/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import LoginForm,RegistrationForm
 
 # Create your views here.
+def home(request): 
+  return render(request,'index.html')
+
+def register(request): 
+  message=None
+  if request.method == 'POST': 
+    form = RegistrationForm(request.POST)
+    if form.is_valid(): 
+      user = form.save()
+      message='User created'
+      return redirect('/account/login/')
+    else: 
+      message='form is not valid'
+  else: 
+    form = RegistrationForm()
+  return render(request,'two_factor/register.html',{"form":form})
+
