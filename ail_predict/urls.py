40fefd7006c0d471efd5cb3df0adb7832bfed8fa
Alfred Warui <alfredwarui17@gmail.com>
2021-11-03 20:35:15 +0300

    add jwt urls as well as app

--- ail_predict/urls.py
+++ ail_predict/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from two_factor.urls import urlpatterns as tf_urls
+# from rest_framework_simplejwt.views import (
+#     # TokenObtainPairView,
+#     TokenRefreshView,
+# )
+# from prediction.views import MyObtainTokenPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('prediction.urls')),
     path('',include(tf_urls)),
+    # path('api/token/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
