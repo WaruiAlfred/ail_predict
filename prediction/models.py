b51ce248925e03a3229c9059365daa221bbee5fe
Alfred Warui <alfredwarui17@gmail.com>
2021-11-03 10:59:42 +0300

    create abstractuser model,its form and register it in admin page

--- prediction/models.py
+++ prediction/models.py
@@ -1,3 +1,9 @@
+# from django.db import models
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser): 
+  is_admin = models.BooleanField('Admin',default=False)
+  is_patient = models.BooleanField('Patient',default=False)
+  is_health_practitioner = models.BooleanField('Health practitioner',default=False)
